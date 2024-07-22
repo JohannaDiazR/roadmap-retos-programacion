c497d37c90b84963c311c5d2d807fbf2f783357a
Johanna Diaz <123778194+JohannaDiazR@users.noreply.github.com>
2024-07-22 17:45:48 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/JohannaDiazR.py
@@ -0,0 +1,26 @@
+# https://python.org/
+
+# Comentario en una línea
+
+"""
+Comentario en varias líneas
+"""
+'''
+Otra manera de comentar varias 
+líneas de código
+'''
+# En python se utiliza el snake_case para crear una variable
+variable = "Variable"
+
+# No hay constantes en python, las variables son mutables = cambian
+
+# Datos Primitivos
+
+cadena_texto = "string"
+integer = 27
+float = 93.6
+bool = True
+bool = False
+
+# Imprimir por consola
+print("¡Hola, Python :) !")
